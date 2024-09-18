f93f4b2ebf64ff17728821f524efdfc1b6893f57
1v4nk0 <1vancherk3s@gmail.com>
2024-09-18 10:47:54 +0100

    added useDebounce hook

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -2,20 +2,26 @@
 import Item from "./Item";
 import { getItems } from "../apiStore";
 import { useLoaderData } from "react-router";
+import { useSelector } from "react-redux";
 import Sidebar from "./Sidebar";
 import { Link } from "react-router-dom";
+import useDebounce from "../useDebounce";
 function Home() {
-  //create getList function then
-
-  //then map through the list to create Item
   const list = useLoaderData();
-  
+  const query = useSelector((state) => state.search.query);
+
+  //create filtered list based on query:
+  const filteredList = useDebounce(
+    () => list.filter((item) => item.name.toLowerCase().includes(query)),
+    500
+  );
+
   return (
     <div className="flex ">
       <Sidebar list={list} />
       <div className="flex-1 flex justify-center mt-[50px]">
         <ul className="grid  grid-cols-1 gap-[40px] md:grid-cols-3 sm:grid-cols-2 p-4">
-          {list.map((item) => (
+          {filteredList.map((item) => (
             <Link key={item.id} to={`/item/${item.id}`}>
               <Item item={item} />
             </Link>
